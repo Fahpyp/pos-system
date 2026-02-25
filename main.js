7604140a8ab7dfa5d33c2342d89922eda021221f
piyaphon <piyaphon.p@chokchaipibul.com>
2026-02-25 14:41:39 +0700

    initial clean commit

--- main.js
+++ main.js
@@ -1,47 +1,25 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const fs = require('fs')
-
-let mainWindow
-const PORT = 3000
-
-function startNextServer() {
-  const serverPath = path.join(
-    process.resourcesPath,
-    'app',                 
-    '.next',
-    'standalone',
-    'server.js'
-  )
-
-  console.log('Loading Next server:', serverPath)
-
-  if (!fs.existsSync(serverPath)) {
-    console.error('❌ server.js not found')
-    return
-  }
-
-  require(serverPath)
-}
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-  })
-
-  mainWindow.loadURL(`http://localhost:${PORT}`)
-}
-
-app.whenReady().then(() => {
-  startNextServer()
-
-  // หน่วงเล็กน้อยพอให้ server boot
-  setTimeout(() => {
-    createWindow()
-  }, 3000)
+autoUpdater.on('checking-for-update', () => {
+  console.log('Checking for update...')
 })
 
-app.on('window-all-closed', () => {
-  app.quit()
+autoUpdater.on('update-available', () => {
+  console.log('Update available.')
+})
+
+autoUpdater.on('update-not-available', () => {
+  console.log('Update not available.')
+})
+
+autoUpdater.on('error', (err) => {
+  console.log('Error in auto-updater:', err)
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+  console.log(`Download speed: ${progressObj.bytesPerSecond}`)
+  console.log(`Downloaded ${progressObj.percent}%`)
+})
+
+autoUpdater.on('update-downloaded', () => {
+  console.log('Update downloaded; will install now')
+  autoUpdater.quitAndInstall()
 })
